724b1ff7faca57a3ade6c15b90e559b6f9fa3247
PRINCE CEEJAY NG <92728956+PRINCE-CEEJAY@users.noreply.github.com>
2025-11-25 13:34:29 +0000

    setting up start screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import Loader from './components/Loader';
-
+import Start from './components/Start';
 export default function App() {
   return (
     <div className='flex flex-col justify-center items-center min-h-screen'>
-      <h1>Home</h1>
       <Loader />
+      <Start />
     </div>
   );
 }
--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -1,3 +1,33 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from './ui/card';
+import { Button } from './ui/button';
+
 export default function Start() {
-  return <div>Start</div>;
+  return (
+    <div>
+      <Card className='w-full max-w-md text-center'>
+        <CardHeader>
+          <CardTitle>WORDLE GAME</CardTitle>
+          <CardDescription>
+            Choose your Options to Start the Game
+          </CardDescription>
+        </CardHeader>
+        <CardContent>{/* Content goes here */}</CardContent>
+        <CardFooter className='flex-col gap-2'>
+          <Button
+            type='submit'
+            className='w-full cursor-pointer'
+          >
+            Start Game Now
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
